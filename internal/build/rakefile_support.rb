1602e7f70fff540735787b0c75aed149481e2ddb
Hongli Lai (Phusion) <hongli@phusion.nl>
2016-12-05 11:48:24 +0100

    Reset the package release number
    They only need to be bumped when releasing updates to the same package versions.

--- internal/build/rakefile_support.rb
+++ internal/build/rakefile_support.rb
@@ -60,12 +60,12 @@ def set_constants_and_envvars
 
   if passenger_enterprise?
     set_constant_and_envvar :PASSENGER_RPM_NAME, "passenger-enterprise"
-    set_constant_and_envvar :PASSENGER_RPM_RELEASE, 9
+    set_constant_and_envvar :PASSENGER_RPM_RELEASE, 2
     set_constant_and_envvar :PASSENGER_TARBALL_NAME, "passenger-enterprise-server"
     set_constant_and_envvar :PASSENGER_APACHE_MODULE_RPM_NAME, "mod_passenger_enterprise"
   else
     set_constant_and_envvar :PASSENGER_RPM_NAME, "passenger"
-    set_constant_and_envvar :PASSENGER_RPM_RELEASE, 8
+    set_constant_and_envvar :PASSENGER_RPM_RELEASE, 1
     set_constant_and_envvar :PASSENGER_TARBALL_NAME, "passenger"
     set_constant_and_envvar :PASSENGER_APACHE_MODULE_RPM_NAME, "mod_passenger"
   end
@@ -74,9 +74,9 @@ def set_constants_and_envvars
   set_constant_and_envvar :PASSENGER_TARBALL, "#{PASSENGER_TARBALL_NAME}-#{PASSENGER_VERSION}.tar.gz"
 
   if passenger_enterprise?
-    set_constant_and_envvar :NGINX_RPM_RELEASE, "9.p#{PASSENGER_VERSION}"
+    set_constant_and_envvar :NGINX_RPM_RELEASE, "2.p#{PASSENGER_VERSION}"
   else
-    set_constant_and_envvar :NGINX_RPM_RELEASE, "8.p#{PASSENGER_VERSION}"
+    set_constant_and_envvar :NGINX_RPM_RELEASE, "1.p#{PASSENGER_VERSION}"
   end
   set_constant_and_envvar :NGINX_RPM_NAME, "nginx"
   set_constant_and_envvar :NGINX_VERSION, PhusionPassenger::PREFERRED_NGINX_VERSION
