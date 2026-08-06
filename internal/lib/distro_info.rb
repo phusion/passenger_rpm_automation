205853366599ef0507bccb98a354773933b83ffe
Camden Narzt <c.narzt@me.com>
2026-08-05 22:59:41 -0600

    fix nginx package version fetching

--- internal/lib/distro_info.rb
+++ internal/lib/distro_info.rb
@@ -5,7 +5,8 @@ require 'nokogiri'
 # internal/scripts/regen_distro_info_script.sh
 
 def numeric(distro)
-  distro.delete_prefix('el').to_i
+  distro = distro.delete_prefix('el') if distro.is_a? String
+  distro.to_i
 end
 
 REDHAT_ENTERPRISE_DISTRIBUTIONS = {
@@ -28,19 +29,20 @@ def latest_nginx_available_parts(release, distro)
     when :rhel  then "https://cdn-ubi.redhat.com/content/public/ubi/dist/ubi#{numeric(release)}/#{numeric(release)}"
   end
 
-  if release < 10
+  #if release < 10
   url += case distro
          when :rocky then "/AppStream/#{arch}/os/Packages/n/"
          when :alma  then "/AppStream/#{arch}/os/Packages/"
          when :rhel  then "/#{arch}/appstream/os/Packages/n/"
          end
-  else
-    url += case distro
-           when :rocky then "/devel/#{arch}/os/Packages/n/"
-           when :alma  then "/CRB/#{arch}/os/Packages/"
-           when :rhel  then "/#{arch}/appstream/os/Packages/n/" # path for nginx-mod-stream because nginx-mod-devel not in ubi10
-           end
-  end
+  # else
+  # URLS for nginx-mod-devel
+  #   url += case distro
+  #          when :rocky then "/devel/#{arch}/os/Packages/n/"
+  #          when :alma  then "/CRB/#{arch}/os/Packages/"
+  #          when :rhel  then "" # doesn't exist
+  #          end
+  # end
 
   cache_file = "/tmp/#{distro}_#{release}_nginx_version.txt"
   if !File.exist?(cache_file) || ((Time.now - 60*60*24) > File.mtime(cache_file))
@@ -54,7 +56,7 @@ def latest_nginx_available_parts(release, distro)
       end
     end
     version_parts = doc
-                      .css('a[href^="nginx-mod-"]') # cannot use full nginx-mod-devel name because ubi10 doesn't include that package for some reason
+                      .css('a[href^="nginx-"]') # cannot use full nginx-mod-devel name because ubi10 doesn't include that package for some reason, use 'nginx' instead
                       .map { |el| el['href'] }
                       .reject { |s| [ "-mod-", ".noarch.", "-core-" ].any? { |p| s.include?(p) } }
                       .map { |url| URI.decode_uri_component(url).delete_suffix(".#{arch}.rpm").split('-').slice(1..) }
